200b2a5f6000a6db76dc0fb5ab6adec4c757f1da
Domicio <testedomicio@hotmail.com>
2015-10-25 16:48:53 -0200

    java Partilho

---
+++ src/cprincipal/CPrincipal.java
@@ -0,0 +1,15 @@
+package cprincipal;
+
+import javax.swing.JFrame;
+
+public class CPrincipal {
+    public static void main (String args []){
+        JFrame janela = new JFrame();
+        janela.setSize(500,300);
+        
+        janela.setTitle("Calculadora");
+        
+        janela.setVisible(true);
+    }
+    
+}
